7f9fb59f5021314e43343759f278c71191955f89
loriyuhv <2385923602@qq.com>
2025-09-21 12:46:28 +0800

    feat: 案例：防止CPU占用100% 参考：黑马程序员p03.021

---
+++ JavaJucStudy/chapter-01-study/src/main/java/com/wsw98/Test0901.java
@@ -0,0 +1,26 @@
+package com.wsw98;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author loriyuhv
+ * @date 2025/9/21 12:38
+ * @description 案例：防止CPU占用100%
+ */
+@Slf4j
+public class Test0901 {
+    public static void main(String[] args) {
+        for(int i=0;i<16;i++){
+            new Thread(()->{
+                log.debug("{}创建了。", Thread.currentThread().getName());
+                while(true){
+                    // try {
+                    //     Thread.sleep(20);
+                    // } catch (InterruptedException e) {
+                    //     throw new RuntimeException(e);
+                    // }
+                }
+            }).start();
+        }
+    }
+}
