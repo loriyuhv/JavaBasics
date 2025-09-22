f1cc17b4e75fc56a3777e0c65b3e8f9c98020ef7
loriyuhv <2385923602@qq.com>
2025-09-22 16:07:17 +0800

    feat: 测试Map和Hashtable插入10000000条数据的效率 待优化 todo

---
+++ JavaJucStudy/chapter-01-study/src/main/java/com/wsw99/test04/Test04.java
@@ -0,0 +1,15 @@
+package com.wsw99.test04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author loriyuhv
+ * @date 2025/9/22 16:03
+ * @description 测试Map和Hashtable插入10000000条数据的效率
+ */
+public class Test04 {
+    public static void main(String[] args) {
+
+    }
+}
