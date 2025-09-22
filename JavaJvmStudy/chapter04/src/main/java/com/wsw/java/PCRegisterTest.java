9fe426b1ef9f62bb85c3c08c309cb2787767b15a
loriyuhv <2385923602@qq.com>
2025-09-23 06:38:18 +0800

    feat: JVM 程序计数器 PC Register

---
+++ JavaJvmStudy/chapter04/src/main/java/com/wsw/java/PCRegisterTest.java
@@ -0,0 +1,19 @@
+package com.wsw.java;
+
+/**
+ * @author loriyuhv
+ * @date 2025/9/23 5:55
+ * @description 反编译指令：javap -verbose .\PCRegisterTest.class
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "abc";
+
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
