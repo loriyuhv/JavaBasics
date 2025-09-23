4243a300e7004b55357db01a4d40a34d68c7b011
loriyuhv <2385923602@qq.com>
2025-09-23 12:58:08 +0800

    feat: 虚拟机栈帧——操作数栈

---
+++ JavaJvmStudy/chapter05/src/main/java/com/wsw/java1/OperandStackTest.java
@@ -0,0 +1,59 @@
+package com.wsw.java1;
+
+/**
+ * @author loriyuhv
+ * @date 2025/9/23 11:10
+ * @description
+ */
+public class OperandStackTest {
+    public static void main(String[] args) {
+        OperandStackTest test = new OperandStackTest();
+        test.add();
+    }
+    public void testAddOperation() {
+        // byte、short、char、boolean都以int型来保存
+        byte i = 15;
+        int j =8;
+        int k = i + j;
+    }
+
+    public int getSum() {
+        int m = 10;
+        int n = 20;
+        int k = m + n;
+        return k;
+    }
+
+    public void testGetSum() {
+        // 获取上一个栈帧返回的结果，并保存在操作数栈中
+        int i = getSum();
+        int j = 10;
+    }
+
+    // 常见的i++和++i的区别
+    public void add() {
+        // 第一类问题
+        int i1 = 10;
+        i1++;
+        int i2 = 10;
+        ++i2;
+
+        // 第二类问题
+        int i3 = 10;
+        int i4 = i3++;
+
+        int i5 = 10;
+        int i6 = ++i5;
+
+        // 第三类问题
+        int i7 = 10;
+        i7 = i7++;
+
+        int i8 = 10;
+        i8 = ++i8;
+
+        // 第四类问题
+        int i9 = 10;
+        int i10 = i9++ + ++i9;
+    }
+}
