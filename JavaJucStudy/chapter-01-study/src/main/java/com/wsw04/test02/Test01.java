8b5d1e3196b1ef0aaf9896a5f761229ccd337f91
loriyuhv <2385923602@qq.com>
2025-09-24 17:08:08 +0800

    feat: JDK15锁状态：无锁01 ==> 轻量级锁（自旋锁）00 ==> 重量锁10

---
+++ JavaJucStudy/chapter-01-study/src/main/java/com/wsw04/test02/Test01.java
@@ -0,0 +1,26 @@
+package com.wsw04.test02;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * @author loriyuhv
+ * @date 2025/9/24 13:08
+ * @description JDK15开始，废弃偏向锁
+ * JDK15锁状态：无锁01 ==> 轻量级锁（自旋锁）00 ==> 重量锁10
+ */
+@Slf4j
+public class Test01 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        // log.debug("\n{}", ClassLayout.parseInstance(cat).toPrintable());
+        synchronized (cat) {
+            // log.debug("\n{}", ClassLayout.parseInstance(cat).toPrintable());
+        }
+        // log.debug("\n{}", ClassLayout.parseInstance(cat).toPrintable());
+    }
+}
+
+class Cat {
+
+}
