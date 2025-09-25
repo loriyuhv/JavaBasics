0cd365bd45ed8e1a9b0cb8671c8f58ec5f5f310d
loriyuhv <2385923602@qq.com>
2025-09-25 08:06:04 +0800

    feat: 增加超时效果 chapter-4-study package com.test; Test20

--- JavaJucStudy/chapter-04-study/src/main/java/com/test/Test20.java
+++ JavaJucStudy/chapter-04-study/src/main/java/com/test/Test20.java
@@ -17,12 +17,17 @@ public class Test20 {
         GuardedObject guardedObject = new GuardedObject();
         Thread t1 = new Thread(() -> {
             // 等待结果
-            Object object = guardedObject.get();
-            log.debug("t1等待结果：{}", object.toString());
+            Object object = guardedObject.get(2000);
+            log.debug("t1等待结果：{}", object);
         }, "t1");
         t1.start();
 
         Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                log.error(e.getMessage());
+            }
             log.debug("t2获取结果");
             guardedObject.complete(new Object());
         }, "t2");
@@ -30,21 +35,38 @@ public class Test20 {
     }
 }
 
+// 增加超时效果
 @Slf4j
 class GuardedObject {
     // 结果
     private Object response;
 
-    // 获取结果
-    public Object get() {
+    /**
+     * 获取结果
+     * @param timeout 等待时间
+     * @return 结果
+     */
+    public Object get(long timeout) {
         synchronized (this) {
+            // 开始时间
+            long start = System.currentTimeMillis();
+            // 经历时间
+            long passedTime = 0;
             // 没有结果
             while (response == null) {
+                // 等待时间
+                long waitTime = timeout -passedTime;
+                // 经历的时间超过了最大等待时间，退出循环
+                if (waitTime <= 0) {
+                    break;
+                }
                 try {
-                    this.wait();
+                    this.wait(waitTime);
                 } catch (InterruptedException e) {
                     log.error(e.getMessage());
                 }
+                // 求得经历时间
+                passedTime = System.currentTimeMillis() - start;
             }
         }
         return response;
