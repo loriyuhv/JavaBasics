7d131c99e38bf969abd45c75d306279ac36a78c1
loriyuhv <2385923602@qq.com>
2025-10-03 15:18:35 +0800

    feat: 对象实例化

---
+++ JavaJvmStudy/chapter10/src/main/java/com/wsw/java/CustomerTest.java
@@ -0,0 +1,12 @@
+package com.wsw.java;
+
+/**
+ * @author loriyuhv
+ * @version 1.0 2025/10/3 15:17
+ * @since 1.0
+ */
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+    }
+}
