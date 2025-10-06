0192c02d63c6567cd3a206a67d00171e38b3a028
loriyuhv <2385923602@qq.com>
2025-10-06 10:26:34 +0800

    chore: 可见性问题：退不出的循环

--- JavaJucStudy/chapter-05-study/src/main/java/com/wsw/java/Test32.java
+++ JavaJucStudy/chapter-05-study/src/main/java/com/wsw/java/Test32.java
@@ -4,29 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * 可见性问题：退不出的循环
- * volatile 对比 synchronized
- * synchronized更重量级，耗费资源更多。
- * 可见性推荐volatile
  *
  * @author loriyuhv
  * @version 1.0 2025/10/6 9:31
- * @since 3.0 解决可见性问题 synchronized
+ * @since 1.0 修改成正确的示例
  */
 @Slf4j
 public class Test32 {
     static boolean run = true;
 
-    // 锁对象
-    public final static Object lock = new Object();
-
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(() -> {
-            while (true) {
-                synchronized (lock) {
-                    if (!run) {
-                        break;
-                    }
-                }
+            while (run) {
 
             }
         }, "t");
@@ -34,8 +23,6 @@ public class Test32 {
 
         Thread.sleep(1000);
         log.debug("停止t");
-        synchronized (lock) {
-            run = false;
-        }
+        run = false;
     }
 }
