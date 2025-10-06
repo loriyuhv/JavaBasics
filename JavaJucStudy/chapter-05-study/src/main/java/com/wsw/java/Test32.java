c09101c21384956297755429e2c5cd0603af48b7
loriyuhv <2385923602@qq.com>
2025-10-06 10:14:38 +0800

    feat: 3.0 解决可见性问题 synchronized

--- JavaJucStudy/chapter-05-study/src/main/java/com/wsw/java/Test32.java
+++ JavaJucStudy/chapter-05-study/src/main/java/com/wsw/java/Test32.java
@@ -4,26 +4,38 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * 可见性问题：退不出的循环
+ * volatile 对比 synchronized
+ * synchronized更重量级，耗费资源更多。
+ * 可见性推荐volatile
  *
  * @author loriyuhv
  * @version 1.0 2025/10/6 9:31
- * @since 2.0 解决可见性问题 关键性 volatile
+ * @since 3.0 解决可见性问题 synchronized
  */
 @Slf4j
 public class Test32 {
-    // 易变
-    volatile static boolean run = true;
+    static boolean run = true;
+
+    // 锁对象
+    public final static Object lock = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(() -> {
-            while (run) {
-                log.debug("t is running.");
+            while (true) {
+                synchronized (lock) {
+                    if (!run) {
+                        break;
+                    }
+                }
+
             }
         }, "t");
         t.start();
 
         Thread.sleep(1000);
         log.debug("停止t");
-        run = false;
+        synchronized (lock) {
+            run = false;
+        }
     }
 }
