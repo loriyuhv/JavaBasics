8c3f10808a3116ad03402adbb51468a7f161db0f
loriyuhv <2385923602@qq.com>
2026-08-14 09:36:25 +0800

    chore(juc:chapter01): 输出日志优化

--- JavaJucStudy/chapter-01-study/src/main/java/com/wsw01/ThreadTest.java
+++ JavaJucStudy/chapter-01-study/src/main/java/com/wsw01/ThreadTest.java
@@ -1,10 +1,13 @@
 package com.wsw01;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author loriyuhv
  * @version 2025/9/20 20:10
  * @since 1.0
  */
+@Slf4j(topic = "c.ThreadTest")
 public class ThreadTest {
     public static void main(String[] args) {
         Thread01 thread01 = new Thread01();
